bc4f0c139b243f89906deb025c1e4c8ada215544
Topeetee <temitopeakinwekomi28@gmail.com>
2023-06-20 15:51:58 -0700

    complete header parser
    Header parser for freecodecamp

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.get("/api/whoami",(req,res)=>{
+  const ipaddress = req.headers.host;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
